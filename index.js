fd53b95b5ee2ee93e337e1e59b9ee2406906ebdb
JevinLeon <jevinleon@gmail.com>
2024-05-06 22:04:15 +0700

    first commit

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+require("dotenv").config();
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const app = express();
-const port = 3000;
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+const port = process.env.PORT || 3000;
 
 app.use("/", (req, res) => {
   res.send("Hello World!");
